bac9c0c63d7ebe6b5c4f1e361ae023d28603781f
Guillermo Andrada <guillermoandrada@gmail.com>
2023-06-09 01:21:43 -0300

    Agregados path para mongoDB

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:" +
+      process.env.MONGOPASSWORD +
+      "@cluster-api.rm472wx.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.use(morgan("dev"));
 
@@ -14,15 +32,15 @@ app.use(bodyParser.urlencoded({ extended: false })); // Para analizar datos de f
 app.use(bodyParser.json()); // Para analizar datos JSON
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     req.header(
-      'Access-Control-Allow-Methods',
-      'PUT, POST, PATCH, DELETE, GET, OPTIONS'
+      "Access-Control-Allow-Methods",
+      "PUT, POST, PATCH, DELETE, GET, OPTIONS"
     );
     return res.status(200).json({});
   }
@@ -34,7 +52,7 @@ app.use("/orders", orderRoutes);
 
 // Error handlers
 app.use((req, res, next) => {
-  const errorHandler = new Error("Not found");
+  const errorHandler = new Error("This is not a valid path");
   errorHandler.status = 404;
   next(errorHandler);
 });
